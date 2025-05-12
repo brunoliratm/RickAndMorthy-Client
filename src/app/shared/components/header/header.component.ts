060e1e36de23e135a11a3aab85f6273fe0817a2f
Marcelo Mesquita <marceloguilherme2010@gmail.com>
2025-05-12 20:45:00 -0300

    feat(header): create header and desktop styles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, ButtonModule ],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
