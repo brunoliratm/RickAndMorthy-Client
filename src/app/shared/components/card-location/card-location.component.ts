3fceb233f57d0663e24d706e23e5f777d9891483
Marcelo Mesquita <marceloguilherme2010@gmail.com>
2025-05-14 20:04:18 -0300

    fix: comment component

--- src/app/shared/components/card-location/card-location.component.ts
+++ src/app/shared/components/card-location/card-location.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Location } from '@core/services/location.service';
-import { LocationDetailDialogComponent } from '@shared/components/dialogs/location-detail-dialog/location-detail-dialog.component';
 import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-card-location',
   standalone: true,
-  imports: [CommonModule, LocationDetailDialogComponent, DialogModule],
+  imports: [CommonModule, DialogModule],
   templateUrl: './card-location.component.html',
   styleUrl: './card-location.component.scss',
 })
