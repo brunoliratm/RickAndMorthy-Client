8974c31f2b87fc9d673d8bcff9b6fd90cd6d2ce1
Marcelo Mesquita <marceloguilherme2010@gmail.com>
2025-05-16 20:01:59 -0300

    feat(characters): adding carousel to improvement interface

--- src/app/features/home/components/character-section/character-section.component.ts
+++ src/app/features/home/components/character-section/character-section.component.ts
@@ -4,25 +4,48 @@ import { Character } from '@core/models/character.model';
 import { CharacterService } from '@core/services/character.service';
 import { CardCharacterComponent } from '@shared/components/cards/card-character/card-character.component';
 import { RouterModule } from '@angular/router';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-character-section',
   standalone: true,
-  imports: [CommonModule, CardCharacterComponent, RouterModule],
+  imports: [
+    CommonModule,
+    CardCharacterComponent,
+    RouterModule,
+    CarouselModule
+  ],
   templateUrl: './character-section.component.html',
   styleUrl: './character-section.component.scss',
 })
 export class CharacterSectionComponent implements OnInit {
   characters: Character[] = [];
 
-  constructor(private characterService: CharacterService) {}
+  responsiveOptions = [
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 1
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 1
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ];
+
+  constructor(private characterService: CharacterService) { }
 
   ngOnInit(): void {
-
     this.characterService
       .getCharacters({})
       .subscribe((res) => {
-      this.characters = res.results.slice(0, 10);
+        this.characters = res.results.slice(0, 10);
       });
   }
 }
