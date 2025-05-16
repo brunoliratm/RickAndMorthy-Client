76a627efcdf8bcfdf7e339017edb8b0adab5c14c
Bruno Magno <bmagnoserver@gmail.com>
2025-05-16 14:59:46 -0300

    feat: replace list components with page components for characters, locations, and episodes

---
+++ src/app/features/episodes/pages/episode-page/episode-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-episode-page',
+  imports: [],
+  templateUrl: './episode-page.component.html',
+  styleUrl: './episode-page.component.scss',
+})
+export class EpisodePageComponent {}
---
+++ src/app/features/locations/pages/location-page/location-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-location-page',
+  imports: [],
+  templateUrl: './location-page.component.html',
+  styleUrl: './location-page.component.scss'
+})
+export class LocationPageComponent {
+
+}
