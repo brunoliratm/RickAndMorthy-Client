6a9de9b44ba74f9c1f91ff49817f5db0e0a10980
Marcelo Mesquita <marceloguilherme2010@gmail.com>
2025-05-19 00:53:56 -0300

    feat(auth): create observables to know when is user logged in

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private tokenKey = 'user_token';
+  private authSubject = new BehaviorSubject<boolean>(this.hasToken());
+  authStatus$: Observable<boolean> = this.authSubject.asObservable();
 
   constructor() {}
 
   login(token: string): void {
     localStorage.setItem(this.tokenKey, token);
+    this.authSubject.next(true);
   }
 
   logout(): void {
     localStorage.removeItem(this.tokenKey);
+    this.authSubject.next(false);
   }
 
   isAuthenticated(): boolean {
+    return this.authSubject.getValue();
+  }
+
+  private hasToken(): boolean {
     const token = localStorage.getItem(this.tokenKey);
     return !!token;
   }
